b185c365449f506255f8b0a3aa0dabd053082ab6
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2022-05-15 14:48:09 -0300

    first commit setup project

---
+++ src/styles/theme.ts
@@ -0,0 +1,26 @@
+import { extendTheme } from "@chakra-ui/react";
+
+export const theme = extendTheme({
+  colors: {
+    yellow: {
+      "400": "#FFBA08",
+    },
+    gray: {
+      "500": "#47585B",
+      "400": "#999999",
+      "300": "#DADADA",
+      "200": "#F5F8FA",
+    },
+  },
+  fonts: {
+    heading: "Poppins",
+    body: "Poppins",
+  },
+  styles: {
+    global: {
+      body: {
+        bg: "gray.200",
+      },
+    },
+  },
+});
