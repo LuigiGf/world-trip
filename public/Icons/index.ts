1f29dad4803e8339aeba06692c0fa2667522e447
LuigiGF <luigi.gottardello.fonseca@gmail.com>
2022-05-15 16:25:57 -0300

    feat(pages index): added divider between contents

--- public/Icons/index.ts
+++ public/Icons/index.ts
@@ -1,2 +1,5 @@
 export * from "./Beach";
 export * from "./Cup";
+export * from "./Globe";
+export * from "./Modern";
+export * from "./Museum";
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,62 @@
-import { Img } from "@chakra-ui/react";
-import { Cup } from "../../public/Icons";
+import { Divider, Grid, GridItem, Img } from "@chakra-ui/react";
+import { useMemo } from "react";
+import { Cup, Beach, Globe, Modern, Museum } from "../../public/Icons";
 import Benefit from "../components/Benefit";
 import Header from "../components/Header";
 
 export default function Home() {
+  const benefitItems = useMemo(
+    () => [
+      {
+        title: "vida noturna",
+        svg: <Cup />,
+      },
+      {
+        title: "praia",
+        svg: <Beach />,
+      },
+      {
+        title: "moderno",
+        svg: <Modern />,
+      },
+      {
+        title: "clássico",
+        svg: <Museum />,
+      },
+      {
+        title: "e mais...",
+        svg: <Globe />,
+      },
+    ],
+    []
+  );
+
   return (
     <>
       <Header />
       <Img alt="Banner" src="Banner.png" objectFit="cover" w="full" />
-      <Benefit title="Vida Noturna" svg={<Cup />} />
+      <Grid
+        my="20"
+        justifyItems="center"
+        mx="auto"
+        maxW="7xl"
+        templateColumns="repeat(5, 1fr)"
+        gap="6"
+      >
+        {benefitItems.map(({ title, svg }, index) => (
+          <GridItem maxW="max" key={`${title} ${index}`}>
+            <Benefit title={title} svg={svg} />
+          </GridItem>
+        ))}
+      </Grid>
+      <Divider
+        mx="auto"
+        orientation="horizontal"
+        mb="3.25rem"
+        w="130px"
+        borderColor="black"
+        borderBottomWidth="3px"
+      />
     </>
   );
 }
